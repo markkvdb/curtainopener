1703867e5d67c6b5d9deeb1fbbc1fe426d5ef1fc
markkvdb <markvanderbroek@gmail.com>
2017-07-03 16:10:56 +0200

    Motor function completed (hopefully)

--- curtainopener/stepcontroller.py
+++ curtainopener/stepcontroller.py
@@ -5,18 +5,30 @@ from .alarm import Alarm
 
 
 def motor_controller(alarm):
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(17, GPIO.OUT)
-    GPIO.setup(4, GPIO.OUT)
+    number_of_steps = 10
+    duration_motor = 60
 
-    p = GPIO.PWM(17, 5)
-    p.start(0.1)
+    total_time_per_step = int(alarm.seconds_to_last / number_of_steps)
+    motor_time_per_step = int(duration_motor / number_of_steps)
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(4, GPIO.OUT)
+    GPIO.setup(17, GPIO.OUT)
+    GPIO.setup(22, GPIO.OUT)
+
+    GPIO.output(4, not alarm.open)
 
     try:
-        sleep(100)
+        for time in range(0, number_of_steps):
+            p = GPIO.PWM(17, 100)
+            GPIO.output(22, True)
+            p.start(0.1)
+            sleep(motor_time_per_step)
+            p.stop()
+            GPIO.output(22, False)
+            sleep(total_time_per_step - motor_time_per_step)
+
     except KeyboardInterrupt:
-        print('Motor is interrupted.')
         pass
 
-    p.stop()
     GPIO.cleanup()
