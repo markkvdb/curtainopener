67e7c424381a36c5614e7ba6f54a75b29b3d917b
markkvdb <markvanderbroek@gmail.com>
2017-07-03 13:33:07 +0200

    First attempt at stepcontroller

--- curtainopener/stepcontroller.py
+++ curtainopener/stepcontroller.py
@@ -1,5 +1,23 @@
-
+from time import sleep
+import RPi.GPIO as GPIO
 
 
 def motor_controller(to_open):
-    pass
+    GPIO.setmode(GPIO.BCM)
+
+    GPIO.setup(17, GPIO.OUT)
+    GPIO.setup(4, GPIO.OUT)
+
+    output_17 = True
+
+    try:
+        while True:
+            GPIO.output(17, output_17)
+            output_17 = not output_17
+            sleep(0.0015)
+    except KeyboardInterrupt:
+        pass
+
+    GPIO.cleanup()
+
+
