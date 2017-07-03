56273898af569ebebdbde077c32304afab072eb2
markkvdb <markvanderbroek@gmail.com>
2017-07-03 14:51:55 +0200

    Settings initial entry fix

--- curtainopener/database_handler.py
+++ curtainopener/database_handler.py
@@ -47,6 +47,7 @@ def init_db():
     db = get_db()
     with app.open_resource('schema.sql', mode='r') as f:
         db.cursor().executescript(f.read())
+    db.cursor().execute('INSERT INTO settings (value) VALUES (30)')
     db.commit()
 
 
